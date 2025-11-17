49120209c33c8a0799b634dc6ec9aa68e15368ff
Aluno Canoas <aluno.canoas@ensino.ulbra.net>
2025-11-17 20:09:54 -0300

    feat: Criação da classe enrollment

---
+++ src/Enrollment.java
@@ -0,0 +1,23 @@
+public class Enrollment {
+
+    private final Student student;
+    private final Course course;
+
+    public Enrollment(Student student, Course course) {
+        this.student = student;
+        this.course = course;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    @Override
+    public String toString() {
+        return student.getId() + " -> " + course.getCode();
+    }
+}
