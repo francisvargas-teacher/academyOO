4663d214efc47bf70ca26ff8aa6a505e6c469a1e
Aluno Canoas <aluno.canoas@ensino.ulbra.net>
2025-11-17 19:42:06 -0300

    fix/func_loc

--- src/GradeService.java
+++ src/GradeService.java
@@ -1,15 +1,7 @@
 public class GradeService {
 
-    private double nota;
-    private Course curso;
-    private Student aluno;
-
-    public String lancarNota(Course curso, Student aluno, double nota){
-        this.curso = curso;
-        this.aluno = aluno;
-        this.nota = nota;
-
-        return "Notas lançadas com sucesso!\nCurso: "+this.curso+"\nAluno: "+this.aluno+"\nNota: "+this.nota;
-    }//end func lancarNota
+    public String getAllGrades(){
+        return "TODO";
+    }
 
 }//end class
