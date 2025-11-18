b7c9521850dfa7cdb65e385bad8d5da8d20f4a2d
Aluno Canoas <aluno.canoas@ensino.ulbra.net>
2025-11-17 21:53:19 -0300

    todo later

--- src/Grade.java
+++ src/Grade.java
@@ -2,17 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Grade{
-    private List<> notas = new ArrayList<>();
+    private double nota;
     private Course curso;
     private Student aluno;
 
     public String lancarNota(Course curso, Student aluno, double nota){
         this.curso = curso;
         this.aluno = aluno;
-        this.notas.add(nota);
+        this.nota = nota;
 
         return "Notas lançadas com sucesso!\nCurso: "+this.curso+"\nAluno: "+this.aluno+"\nNota: "+this.nota;
     }//end func lancarNota
 
+    public Course getCurso() {
+        return curso;
+    }
+    public Student getAluno() {
+        return aluno;
+    }
 
+    public double getNotas() {
+        return nota;
+    }
 }//end class
--- src/GradeService.java
+++ src/GradeService.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GradeService {
-    public String tapaBuraco(){
-        return "Hello World!";
+
+    private ArrayList<Grade> notas = new ArrayList<Grade>();
+
+    public double mediaNotas(Course curso){
+        int count = 0;
+        double soma = 0;
+
+        for(Grade nota1:notas){
+            if(nota1.getCurso() == curso){
+                for(int i = 0; nota1.getNotas().size() > i; i++){
+                    soma = soma + nota1.getNotas();
+                    count++;
+                }
+            }
+        }
     }
+
+    public void SetGrade(Grade nota){
+        notas.add(nota);
+    }
+
+
+
 }//end class
