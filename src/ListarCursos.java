518b0c20d99dac1de947878a69c650790662c75e
Vinicius <vinicius.casagrande@rede.ulbra.br>
2025-11-17 21:02:55 -0300

    feat: Implementa a listagem de cursos e corrige o nextInt no cadastro

--- src/ListarCursos.java
+++ src/ListarCursos.java
@@ -1,2 +1,17 @@
-public class ListarCursos {
+import java.util.Scanner;
+
+public class ListagemCursos implements MenuAction {
+
+    @Override
+    public String getName() {
+        return "Listar Cursos Cadastrados";
+    }
+
+    @Override
+    public void execute(Scanner scanner, CourseCatalog courseCatalog, StudentRegistry studentRegistry) {
+        System.out.println("--- LISTA DE CURSOS CADASTRADOS ---");
+
+        courseCatalog.listCourses();
+
+    }
 }
